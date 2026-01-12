55bef6d6bf0b4bf4db03192833f186344ce8c0cc
Abishek <abishek28122004@gmail.com>
2026-01-12 14:40:46 +0530

    updated

--- week-02/day01-react-core/src/App.tsx
+++ week-02/day01-react-core/src/App.tsx
@@ -1,20 +1,37 @@
-// import JsxVsHtml from "./concepts/JsxVsHtml";
-// import VirtualDom from "./concepts/VirtualDom";
-// import PropsState from "./concepts/PropsState";
-// import LifecycleHooks from "./concepts/LifecycleHooks";
+import JsxVsHtml from "./concepts/JsxVsHtml";
+import VirtualDom from "./concepts/VirtualDom";
+import PropsState from "./concepts/PropsState";
+import LifecycleHooks from "./concepts/LifecycleHooks";
 import UseStateEffect from "./concepts/UseStateEffect";
 import MemoCallback from "./concepts/MemoCallback";
 
 function App() {
   return (
-    <>
-      {/* <JsxVsHtml />
-      <VirtualDom /> */}
-      {/* <PropsState />
-      <LifecycleHooks /> */}
-      <UseStateEffect />
-      <MemoCallback />
-    </>
+    <div style={{ padding: 12 }}>
+      <section style={{ border: '1px solid #ccc', padding: 12, margin: 8 }}>
+        <JsxVsHtml />
+      </section>
+
+      <section style={{ border: '1px solid #ccc', padding: 12, margin: 8 }}>
+        <VirtualDom />
+      </section>
+
+      <section style={{ border: '1px solid #ccc', padding: 12, margin: 8 }}>
+        <PropsState />
+      </section>
+
+      <section style={{ border: '1px solid #ccc', padding: 12, margin: 8 }}>
+        <LifecycleHooks />
+      </section>
+
+      <section style={{ border: '1px solid #ccc', padding: 12, margin: 8 }}>
+        <UseStateEffect />
+      </section>
+
+      <section style={{ border: '1px solid #ccc', padding: 12, margin: 8 }}>
+        <MemoCallback />
+      </section>
+    </div>
   );
 }
 
